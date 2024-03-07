ca843f9e18c9baec98c1bca0f5f67ce7761b0415
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-03-06 22:41:18 -0800

    final css updates

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -1,13 +1,16 @@
 
 const UserCard = ({ username, score1, score2}) => {
 
+  const intScore1 = parseInt(score1)
+  const intScore2 = parseInt(score2)
+
+  const totalScore = intScore1 + intScore2
+
   return (
     <>
     <div className='user-card'>
         <h3>{username}</h3>
-        <span>Score: {score1} <br></br> <br></br>Score: {score2}</span>
-        <br></br>
-        <br></br>
+        <span>Total Score: {totalScore}<br></br> <br></br>Round 1: {score1} <br></br> <br></br>Round 2: {score2}</span>
     </div>
     </>
   )
