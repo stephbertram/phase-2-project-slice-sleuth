a761181463d26e1d58a27c65a446f0dcff48e953
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-03-07 08:29:24 -0800

    add user score sort

--- src/Components/UserList.jsx
+++ src/Components/UserList.jsx
@@ -1,12 +1,24 @@
-// import {useMemo} from 'react'
 import UserCard from './UserCard'
 
 
 const UserList = ({currentUser, users, handleDeleteUser}) => {
 
-    const mappedUser = users.map(user => (
-        <UserCard key={user.id} user={user} {...user} currentUser={currentUser} handleUserDelete={handleDeleteUser}/>
-    ))
+// Sort users descending by total score
+    const sortedUsers = users.sort((a, b) => {
+      const totalScoreA = parseInt(a.score1) + parseInt(a.score2);
+      const totalScoreB = parseInt(b.score1) + parseInt(b.score2);
+      return totalScoreB - totalScoreA; 
+    });
+  
+    const mappedUser = sortedUsers.map(user => (
+      <UserCard
+        key={user.id}
+        user={user}
+        {...user}
+        currentUser={currentUser}
+        handleUserDelete={handleDeleteUser}
+      />
+    ));
 
   return (
     <div className='users-wrapper'>
